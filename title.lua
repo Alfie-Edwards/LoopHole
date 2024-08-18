1e9b34fdf1509f69fb9bd993b21570a72d01ea35
charlie-wt <cwt1g14@soton.ac.uk>
2024-08-18 14:28:41 +0100

    rest of the title screen

--- title.lua
+++ title.lua
@@ -1,6 +1,13 @@
+title_screen = {
+	state = {
+		dust_spawner = nil,
+	}
+}
+
 function init_title_screen(t_started)
-	camera()
+	camera(-64, -64) -- offset needed for dust spawner
 	music(1)
+	title_screen.state.dust_spawner = _make_dust_spawner(7)
 end
 
 function update_title_screen(t_started)
@@ -8,17 +15,40 @@ function update_title_screen(t_started)
 		return screens.gameplay
 	end
 
+	assert(title_screen.state.dust_spawner ~= nil)
+	title_screen.state.dust_spawner.update(title_screen.state.dust_spawner)
+	title_screen.state.dust_spawner.maybe_spawn(title_screen.state.dust_spawner)
+
 	return screens.title
 end
 
 function draw_title_screen(t_started)
+	-- background colour
+	palt(9, false)  -- colour the orange of the loop as orange
 	cls(0)
 
-	color(9)
-	print_centred("loophole", 61)
-	color(10)
-	print_centred("loophole", 60)
+	-- dust
+	assert(title_screen.state.dust_spawner ~= nil)
+	title_screen.state.dust_spawner.draw(title_screen.state.dust_spawner)
 
+	-- logo
+	local logo_idx = 200
+	local logo_sw = 8
+	local logo_sh = 4
+
+	local logo_y = 40 + sin((t() - t_started) * 0.25) * 5
+
+	local logo_x = 64 - (logo_sw * 4)
+
+	local cam_x = peek2(0x5f28)
+	local cam_y = peek2(0x5f2a)
+
+	logo_x += cam_x
+	logo_y += cam_y
+
+	spr(logo_idx, logo_x, logo_y, logo_sw, logo_sh)
+
+	-- prompt
 	color(1)
 	print_centred("PRESS ANY BUTTON...", 101)
 	if strobe(0.66) then
@@ -31,4 +61,6 @@ end
 
 function cleanup_title_screen(t_started)
 	music(-1)
+
+	title_screen.state.dust_spawner = nil
 end
